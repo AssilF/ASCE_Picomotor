982c2c9d01993eaf005b78d4824c4305b2ea1212
Assil Fer <trt8rtr@gmail.com>
2024-10-11 19:58:58 +0100

    some mess I wish to exit

--- ASCE_Picomotor.cpp
+++ ASCE_Picomotor.cpp
@@ -6,6 +6,39 @@
 #include "inttypes.h"
 #include "string.h"
 #include "hardware/pwm.h"
+#include <math.h>
+
+#define system_freq 125e6
+#define target_motor_freq 80e3
+#define motor_PWM_Freq_div ((system_freq)/(target_motor_freq))
+#define MG996r_ferq 50 //50-60 ig ?
+#define SG90_freq 50
+#define MG996r_PWM_Freq_div ((system_freq)/(MG996r_ferq))
+#define SG90_PWM_Freq_div ((system_freq)/(SG90_freq))
+#define us_to_pulse_count(us,freq) ((us*65536)/((1e6/freq)))
+
+#define MG996r_maximum_pulse_width 2000 //in µs
+#define MG996r_minimum_pulse_width 1000
+
+#define SG90_maximum_pulse_width 2000
+#define SG90_minimum_pulse_width 1000
+
+#define servo_angle(degrees,min_pulse,max_pulse,freq,max_angle) (mapRange(0,max_angle,us_to_pulse_count(min_pulse,freq),us_to_pulse_count(max_pulse,freq),degrees))
+
+int mapRange(int a1,int a2,int b1,int b2,int s) {
+  return b1 + (s-a1)*(b2-b1)/(a2-a1);
+}
+
+int constrain(int x, int a, int b) {
+    if(x < a) {
+        return a;
+    }
+    else if(b < x) {
+        return b;
+    }
+    else
+        return x;
+}
 
 int left_top_motor_speed;
 int left_bot_motor_speed;
@@ -55,18 +88,29 @@ int main()
 {   
     stdio_init_all();
     
-    LT_mot_pwm_config=pwm_get_default_config();
-    LB_mot_pwm_config=pwm_get_default_config();
-    RT_mot_pwm_config=pwm_get_default_config();
-    RB_mot_pwm_config=pwm_get_default_config();
-    arm_extension_p_pwm_config=pwm_get_default_config();
-    arm_extension_n_pwm_config=pwm_get_default_config();
-    arm_servo_pwm_config=pwm_get_default_config();
-    elbow_servo_pwm_config=pwm_get_default_config();
-    pitch_servo_pwm_config=pwm_get_default_config();
-    yaw_servo_pwm_config=pwm_get_default_config(); 
+    left_side_motor_pwm_config=pwm_get_default_config();
+    right_side_motor_pwm_config=pwm_get_default_config();
+    arm_extension_pwm_config=pwm_get_default_config();
+    arm_rotation_pwm_config=pwm_get_default_config();
+    arm_elbow_servo_pwm_config=pwm_get_default_config();
+    pitch_yaw_servo_pwm_config=pwm_get_default_config();
+    grip_servo_pwm_config=pwm_get_default_config(); 
 
-    //lez start configuring this stuff *sigh* (first commit commence here)
+    //lez start configuring this stuff *sigh* (first commit commence here) //default PWM clock speed is about 125MHz so do your math accordingly (I am talking to you my future self)
+    pwm_config_set_clkdiv(&left_side_motor_pwm_config ,motor_PWM_Freq_div); //this gives us a frequency of 80KHz (aka one wrap each 0.0008). . . but like, hold up, when will the pwm register wrap ? (I beleive the counter buffer is like, 16 bits ? about up to 65,536)
+    pwm_config_set_clkdiv(&right_side_motor_pwm_config,motor_PWM_Freq_div);
+    pwm_config_set_clkdiv(&arm_extension_pwm_config,motor_PWM_Freq_div);
+    pwm_config_set_clkdiv(&arm_rotation_pwm_config,motor_PWM_Freq_div);
+    pwm_config_set_clkdiv(&arm_elbow_servo_pwm_config,MG996r_PWM_Freq_div);
+    pwm_config_set_clkdiv(&pitch_yaw_servo_pwm_config,SG90_PWM_Freq_div);
+    pwm_config_set_clkdiv(&grip_servo_pwm_config,SG90_PWM_Freq_div); //I know what you think about this, why everyone got its own variable, answer is, idk man I may want to tweak things seperately, but virtually we only needed 2 profiles or configs
+
+    pwm_init(left_side_motor_pwm_slice,&left_side_motor_pwm_config,0);
+    pwm_init(right_side_motor_pwm_slice,&right_side_motor_pwm_config,0);
+    pwm_init(arm_extension_pwm_slice,&arm_extension_pwm_config,0);
+    pwm_init(arm_rotation_pwm_slice,&arm_rotation_pwm_config,0);
+    pwm_init(pitch_yaw_servo_pwm_slice,&pitch_yaw_servo_pwm_config,0);
+    pwm_init(grip_servo_pwm_slice,&grip_servo_pwm_config,0);
 
     gpio_init(25);
     gpio_set_dir(25,1);
@@ -111,9 +155,33 @@ int main()
     Wire.onRequest(answer);
     Wire.begin(0x17);
     
+    pwm_set_enabled(left_side_motor_pwm_slice,1);
+    pwm_set_enabled(left_side_motor_pwm_slice,1);
+    pwm_set_enabled(arm_elbow_servo_pwm_slice,1);
+
     while (true) 
     {
+        sleep_us(50);
+        static int i;
+        static int t;
+
+
+        for(int a=0; a<=180; a++){
+        pwm_set_gpio_level(arm_servo_pin,1639);
+        i++;
+        t+=10;
         sleep_ms(50);
+        pwm_set_both_levels(left_side_motor_pwm_slice,i,t);
+        }
+        for(int a=180; a>0; a--){
+        pwm_set_gpio_level(arm_servo_pin,);
+        sleep_ms(50);
+                i++;
+        t+=10;
+                pwm_set_both_levels(left_side_motor_pwm_slice,i,t);
+        }
+
+
         unpack_frame();
     }
 }
--- include/ASCE_Picomotor.h
+++ include/ASCE_Picomotor.h
@@ -1,42 +1,50 @@
-#define left_top_motor_p_pin 11 //Each driven @ 14.4 volts 
+#include "hardware/pwm.h" 
+
+#define left_top_motor_p_pin 15 //Each driven @ 14.4 volts 
 #define left_top_motor_n_pin 16
-#define left_top_motor_PWM_pin 12
+#define left_top_motor_PWM_pin 2//PWM1A
 
-#define right_top_motor_p_pin 17
-#define right_top_motor_n_pin 18
-#define right_top_motor_PWM_pin 13
+#define left_bot_motor_p_pin 17
+#define left_bot_motor_n_pin 18
+#define left_bot_motor_PWM_pin 3//PWM1B
 
-#define left_bot_motor_p_pin 19
-#define left_bot_motor_n_pin 20
-#define left_bot_motor_PWM_pin 14
+
+#define right_top_motor_p_pin 19
+#define right_top_motor_n_pin 20
+#define right_top_motor_PWM_pin 4//PWM2A
 
 #define right_bot_motor_p_pin 21
 #define right_bot_motor_n_pin 22
-#define right_bot_motor_PWM_pin 15
+#define right_bot_motor_PWM_pin 5//PWM2B
 
 
-#define arm_rotation_motor_p_pin 2  //..
-#define arm_rotation_motor_n_pin 3  //..
-#define arm_extension_motor_p_pin 4 //..
-#define arm_extension_motor_n_pin 5 //All these must be PWM
-#define arm_servo_pin 6             //MG996r Check in the datasheet for the servo timings and frequencies 
-#define elbow_servo_pin 7           //MG996r
-#define pitch_servo_pin 8           //SG90
-#define yaw_servo_pin 9             //SG90
-#define grip_servo_pin 10        //SG90
+#define arm_rotation_motor_p_pin 6  //PWM3A
+#define arm_rotation_motor_n_pin 7  //PWM3B
+#define arm_extension_motor_p_pin 8 //PWM4A
+#define arm_extension_motor_n_pin 9 //PWM4B
+#define arm_servo_pin 10           //PWM5A MG996r Check in the datasheet for the servo timings and frequencies 
+#define elbow_servo_pin 11         //PWM5B (if different properties put on PWM0A in another pin) MG996r //These two may have different pwm properties but it is to be tested
+#define pitch_servo_pin 12         //PWM6A SG90
+#define yaw_servo_pin 13           //PWM6B SG90
+#define grip_servo_pin 14          //PWM7A SG90
 
-pwm_config LT_mot_pwm_config;
-pwm_config LB_mot_pwm_config;
-pwm_config RT_mot_pwm_config;
-pwm_config RB_mot_pwm_config;
-pwm_config arm_extension_p_pwm_config;
-pwm_config arm_extension_n_pwm_config;
-pwm_config arm_rotation_p_pwm_config;
-pwm_config arm_rotation_n_pwm_config;
-pwm_config arm_servo_pwm_config;
-pwm_config elbow_servo_pwm_config;
-pwm_config pitch_servo_pwm_config;
-pwm_config yaw_servo_pwm_config; //can be upgraded with 4 more servos or motors ig ? but pin configs must change so
+pwm_config left_side_motor_pwm_config; //the slice of the pwm controller for this side is PWM1 
+pwm_config right_side_motor_pwm_config; //the slice of the pwm controller for this side is PWM2
+
+pwm_config arm_rotation_pwm_config;   //Slice PWM3
+pwm_config arm_extension_pwm_config;  //Slice PWM4
+
+pwm_config arm_elbow_servo_pwm_config; //Slice PWM5
+pwm_config pitch_yaw_servo_pwm_config; //Slice PWM6
+pwm_config grip_servo_pwm_config;      //Slice PWM7 can be upgraded with 4 more servos or motors ig ? but pin configs must change so
+
+#define left_side_motor_pwm_slice 1
+#define right_side_motor_pwm_slice 2
+#define arm_rotation_pwm_slice 3
+#define arm_extension_pwm_slice 4
+#define arm_elbow_servo_pwm_slice 5
+#define pitch_yaw_servo_pwm_slice 6
+#define grip_servo_pwm_slice 7
 
 struct control_frame
 {
